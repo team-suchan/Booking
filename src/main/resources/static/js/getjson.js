238fedc0cd99160387fcc66dfcf87508f7376e06
NewBoy <1204992207@qq.com>
2018-10-31 00:02:03 +0800

    更新主页、订票

--- src/main/resources/static/js/getjson.js
+++ src/main/resources/static/js/getjson.js
@@ -4,23 +4,24 @@ $(document).ready(function() {
 		type: "GET",
 		success: function(data) {
 			var list = "";
-			for(var i = 0; i < data.data.length; i++) {
-				list += 
+			for(var i = 0; i < data.data.activityList.length; i++) {
+				var time = new Date(data.data.activityList[i].activityDate * 1000);
+				var timeType = time.getFullYear() + "." + time.getMonth() + "." + time.getDate() + " " + time.getHours() + ":" + time.getMinutes()
+				list +=
 					"<div class='project-list-item'>" +
-					"<a href='http://www.baidu.com'>"+
-					"<div class='project-list-item-img' style='background-image: url(" + data.data[i].activityIcon + ");'></div>" +
+					"<a href='http://www.baidu.com'>" +
+					"<div class='project-list-item-img' style='background-image: url(" + data.data.activityList[i].activityIcon + ");'></div>" +
 					"<div class='project-list-item-detail'>" +
-					"<div class='project-list-item-title'>" + data.data[i].activityName + "</div>" +
-					"<div class='project-list-item-time'><span class='icon time-icon'></span>" + data.data[i].activityDate + "</div>" +
+					"<div class='project-list-item-title'>" + data.data.activityList[i].activityName + "</div>" +
+					"<div class='project-list-item-time'><span class='icon time-icon'></span>" + data.data.activityList[i].activityDate + "</div>" +
 					"<div class='project-list-item-address'><span class='icon address-icon'></span>" +
-					"<span class='venue-name-and-address'>" + data.data[i].activityDescribe + "</span></div>" +
+					"<span class='venue-name-and-address'>" + data.data.activityList[i].activityAddress + "</span></div>" +
 					"<div class='project-list-item-price'>" +
-					"<div class='not-free'><span class='price-symbol'></span> <span class='price'>" + data.data[i].ownerId + "</span></div>" +
+					"<div class='not-free'><span class='price-symbol'></span> <span class='price'>可预订</span></div>" +
 					"</div>" +
 					"</div>" +
-					"</a>"+
+					"</a>" +
 					"</div>"
-					
 			};
 			$(".project-list").append(list);
 		}
